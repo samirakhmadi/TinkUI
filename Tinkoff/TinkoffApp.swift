f1d60e24581a82bccb918ffb07ca476b9d91ec45
samirakhmadi <samirakhmadi@gmail.com>
2023-06-19 14:31:04 +0300

    pet
    SwiftUI pet tinkoff

---
+++ Tinkoff/TinkoffApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinkoffApp.swift
+//  Tinkoff
+//
+//  Created by Samir Akhmadi on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TinkoffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
